6b762a6488a3a4c06ce560c1cfe855a11bd4f37f
BosshuBots <ajmalitsme@gmail.com>
2026-02-18 17:00:53 +0530

    Create admins.py

---
+++ database/admins.py
@@ -0,0 +1,23 @@
+from .connection import db
+from config import OWNER_ID
+
+col = db.admins
+
+async def add_admin(user_id: int):
+    await col.update_one(
+        {"user_id": user_id},
+        {"$set": {"user_id": user_id}},
+        upsert=True
+    )
+
+async def remove_admin(user_id: int):
+    await col.delete_one({"user_id": user_id})
+
+async def is_admin(user_id: int):
+    if user_id == OWNER_ID:
+        return True
+
+    return await col.find_one({"user_id": user_id}) is not None
+
+async def get_all_admins():
+    return col.find({})
